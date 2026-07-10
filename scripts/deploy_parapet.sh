dbf79d5f3e9461b67d67355283cef849e322a8d5
Steven <citychip@gmail.com>
2026-07-10 16:24:13 +0200

    docs: Sprint 28.5 uncap tracker shipped

--- scripts/deploy_parapet.sh
+++ scripts/deploy_parapet.sh
@@ -38,6 +38,7 @@ FILES=(
   "src/components/MtfCandleChart.tsx"
   "src/components/TimelinePanel.tsx"
   "src/components/RiskPanel.tsx"
+  "src/components/UncapTracker.tsx"
   "src/components/positions/PositionCards.tsx"
   "src/components/system/UniverseSection.tsx"
   "src/components/system/ConnectionsSection.tsx"
